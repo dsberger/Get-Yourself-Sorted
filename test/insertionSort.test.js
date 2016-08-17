63a1d8b88860ba2b10690868926e0b371ed79215
Dan Berger <dsberger@gmail.com>
2016-08-16 19:15:15 -0700

    corrects leftover bubble sort test name

--- test/insertionSort.test.js
+++ test/insertionSort.test.js
@@ -2,7 +2,7 @@ var cases = require('./cases')
 var expect = require('chai').expect
 var sorting = require('../lib/sorting')
 
-describe('Bubble Sort', () => {
+describe('Insertion Sort', () => {
   it('sorts an array of ten', (done) => {
     expect(sorting.insertionSort(cases.ten)).to.deep.equal(cases.tenSorted)
     done()
